fe39000c9540e70f585873ba82f6dc908acf16d3
linwenze <892639098@qq.com>
2018-10-31 22:14:55 +0800

    [fix]

--- src/routes.js
+++ src/routes.js
@@ -4,7 +4,7 @@ import Router from 'vue-router';
 import indexPage from './components/header.vue'
 import homePage from './views/home.vue'
 import aboutPage from './views/about.vue'
-
+///fdssdffdsdfsdfsr
 Vue.use(Router)
 
 export default new Router({
